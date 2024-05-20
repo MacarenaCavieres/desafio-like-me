c4775446e24dc6bb1701db937254b1fb72922099
MCavieres <maca.c.91@gmail.com>
2024-05-20 19:30:03 -0400

    creacion de queries.sql

--- database/queries.sql
+++ database/queries.sql
@@ -7,3 +7,8 @@ CREATE TABLE POSTS (
 	descripcion VARCHAR(255),
 	likes INT DEFAULT 0 CHECK (LIKES >= 0)
 );
+
+INSERT INTO POSTS (usuario, url, descripcion) VALUES 
+('user1', 'https://fastly.picsum.photos/id/231/500/500.jpg?hmac=1esUfM6PFf1cma0OUojGMBAjfrNQ_0Hmir8wvuJtj6Y', 'descripcion1'),
+('user2', 'https://fastly.picsum.photos/id/261/500/500.jpg?hmac=ezWMIhZgsZogKMR2UTydJ6yjX2G_bvspYtAkXQJ37Lo', 'descripcion2'),
+('user3', 'https://fastly.picsum.photos/id/133/500/500.jpg?hmac=mvAWcFjxl48pXjpOl4Ukw72TUxTLO4njcBCV-9nMFNU', 'descripcion3');
